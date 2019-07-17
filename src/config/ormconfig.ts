b1a7e9f2f57ee6abf9239590697474725de3864d
briefguo <gyj9094@gmail.com>
2019-07-17 13:52:56 +0800

    docs(entities-path): 为entities-path提供描述

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,7 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-console.log(process.env.MYSQL_URL);
-
 export const ormConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.MYSQL_URL || '127.0.0.1',
@@ -12,7 +10,14 @@ export const ormConfig: TypeOrmModuleOptions = {
   // https://github.com/typeorm/typeorm/issues/682
 
   // https://github.com/nestjs/nest/issues/1138#issuecomment-425806220
-  entities: ['dist/**/**.entity.{ts,js}'],
+
+  // README
+  // 这里src能在生产环境生效，是因为生产环境docker的容器目录
+  // Dockerfile
+  // ...
+  // COPY ./dist ./src
+  // ...
+  entities: ['src/**/**.entity.{ts,js}'],
   // debug: true,
   // dropSchema: true,
   synchronize: true,
